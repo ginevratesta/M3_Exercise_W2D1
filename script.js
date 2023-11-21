eafbe93095faec234a6d17e4455143b9fed34223
Ginevra Testa <ginevra99testaa@gmail.com>
2023-11-21 17:18:34 +0100

    aesthetics

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const playButtons = document.querySelectorAll('.play-button');
-    playButtons.forEach(function (button) {
-      button.addEventListener('click', function () {
-        const card = this.closest('.card');
-        const audio = card.querySelector('.audio-player');
+function setMusicPlayer() {
+    // Selezioniamo tutti i pulsanti "Play"
+    const playButtons = document.querySelectorAll(".play-button");
+    // Variabile per tenere traccia dell'audio attualmente in riproduzione
+    let currentAudio = null;
   
+    // Per ogni pulsante "Play"
+    for (let i = 0; i < playButtons.length; i++) {
+      playButtons[i].addEventListener("click", function () {
+        // Trova l'elemento card relativo al pulsante cliccato
+        const card = this.closest(".card");
+        // Trova l'elemento audio all'interno della card
+        const audio = card.querySelector(".audio-player");
+  
+        // Se c'è un audio diverso da quello attualmente in riproduzione, mettilo in pausa e riavvolgi
+        if (currentAudio && currentAudio !== audio) {
+          currentAudio.pause();
+          currentAudio.currentTime = 0;
+          // Cambiamo il testo del pulsante precedente in "Play" quando viene interrotto
+          const previousButton = currentAudio.parentElement.querySelector(".play-button");
+          if (previousButton) {
+            previousButton.textContent = "Play";
+          }
+        }
+  
+        // Se l'audio è in pausa, avvialo e cambia il testo del pulsante in "Pause"
         if (audio.paused) {
           audio.play();
-          this.textContent = 'Pause'; // Cambia il testo del pulsante in "Pause" quando l'audio è in riproduzione
-        } else {
+          this.textContent = "Pause";
+          currentAudio = audio;
+        } else { // Altrimenti, metti in pausa l'audio e cambia il testo del pulsante in "Play"
           audio.pause();
-          audio.currentTime = 0; // Riavvolgi l'audio alla posizione iniziale quando viene messo in pausa
-          this.textContent = 'Play'; // Cambia il testo del pulsante in "Play" quando l'audio è in pausa
+          audio.currentTime = 0;
+          this.textContent = "Play";
+          currentAudio = null;
         }
+  
+        // Gestione dell'evento onended quando la traccia audio termina
+        audio.onended = function () {
+          playButtons[i].textContent = "Play"; // Cambia il testo in "Play" quando la traccia audio termina
+        };
       });
-    });
-  });
+    }
+  }
+  
+  // Richiamiamo la funzione per impostare il lettore musicale
+  setMusicPlayer();
   
